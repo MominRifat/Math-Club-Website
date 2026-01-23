b37e5557ee708e264ba883426dca0fcd144e98d1
MOminRifat <mominrifat8@gmail.com>
2026-01-23 18:03:36 +0600

    view

--- mathclubweb/contact/views.py
+++ mathclubweb/contact/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def contact(request):
+    return HttpResponse("This is the contact page.")
--- mathclubweb/joinus/views.py
+++ mathclubweb/joinus/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def joinus(request):
+    return HttpResponse("This is the join us page.")
