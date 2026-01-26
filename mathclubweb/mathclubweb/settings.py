f7faaafeb66e721aae667761bbd81281a0e6eb1b
MOminRifat <mominrifat8@gmail.com>
2026-01-26 21:27:31 +0600

    root

--- mathclubweb/mathclubweb/settings.py
+++ mathclubweb/mathclubweb/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
